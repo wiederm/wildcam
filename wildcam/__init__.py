15878db0b3a8122724ea720c9e74bc4504490073
wiederm <marcus.wieder@gmail.com>
2022-03-01 20:22:17 +0100

    the rest

---
+++ wildcam/__init__.py
@@ -0,0 +1,11 @@
+"""Recognizing animals on video streams"""
+
+# Add imports here
+from .wildcam import *
+
+# Handle versioneer
+from ._version import get_versions
+versions = get_versions()
+__version__ = versions['version']
+__git_revision__ = versions['full-revisionid']
+del get_versions, versions
